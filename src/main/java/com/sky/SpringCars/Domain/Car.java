dcd74eae3ec8294074758b4512cecd2d60db72e7
kamran-naseem97 <k4mr4n1@hotmail.co.uk>
2023-08-08 12:32:03 +0100

    mockito tests

--- src/main/java/com/sky/SpringCars/Domain/Car.java
+++ src/main/java/com/sky/SpringCars/Domain/Car.java
@@ -2,6 +2,7 @@ package com.sky.SpringCars.Domain;
 
 import javax.persistence.*;
 import javax.persistence.criteria.CriteriaBuilder;
+import java.util.Objects;
 
 @Entity
 public class Car {
@@ -11,6 +12,9 @@ public class Car {
     @Column(unique = false, nullable = true)
     private String makeModel;
     private Integer power;
+
+    @ManyToOne(targetEntity = storageGarage.class)
+    private storageGarage garage;
 
     public Car( String makeModel, Integer power) {
         this.makeModel = makeModel;
@@ -51,11 +55,32 @@ public class Car {
         this.power = power;
     }
 
+    public storageGarage getGarage() {
+        return garage;
+    }
+
+    public void setGarage(storageGarage garage) {
+        this.garage = garage;
+    }
+
     @Override
     public String toString() {
         return "Car{" +
                 "makeModel='" + makeModel + '\'' +
                 ", power=" + power +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return id == car.id && Objects.equals(makeModel, car.makeModel) && Objects.equals(power, car.power);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, makeModel, power);
     }
 }
